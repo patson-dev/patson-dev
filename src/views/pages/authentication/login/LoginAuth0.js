8bf838574a281fceb94bd194735e31f80f265daf
patson-dev <63660201+patson-dev@users.noreply.github.com>
2020-11-22 18:42:42 -0500

    one

--- src/views/pages/authentication/login/LoginAuth0.js
+++ src/views/pages/authentication/login/LoginAuth0.js
@@ -1,9 +1,7 @@
 import React from "react"
 import { CardBody, Button } from "reactstrap"
-import { useAuth0 } from "../../../../authServices/auth0/auth0Service"
 
 const LoginAuth0 = props => {
-  let { isAuthenticated, loginWithRedirect } = useAuth0()
 
   return (
     <React.Fragment>
@@ -11,16 +9,6 @@ const LoginAuth0 = props => {
         <Button.Ripple
           color="primary"
           className="mt-1"
-          onClick={ e => {
-            e.preventDefault()
-
-            if (!isAuthenticated){
-              loginWithRedirect()
-            }
-            else{
-              console.log('Already Logged In')
-            }
-          }}
         >
           Login With Auth0
         </Button.Ripple>
